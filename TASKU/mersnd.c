8991fc5d28142740b74fa9b1b393bb6f6354e6c1
akshayraajput2005 <akshayraajput2005@gmail.com>
2025-08-29 18:21:04 +0530

    Add files via upload

---
+++ TASKU/mersnd.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    //area of the rectangle 
+    int length, breadth ;
+  printf(" write length \n " ); 
+  scanf("%d \n ", &lenghth); 
+  
+  printf(" write breadth \n");
+  scanf("%d \n ", & breadth);
+
+  printf(" area of the rectangle ", length * breadth );
+   return 0;
+
+}
+
+
+
+
+
+
+
